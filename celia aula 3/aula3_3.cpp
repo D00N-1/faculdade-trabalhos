480dba6de5d73072a484a1d6cd3f607f1aa07db9
Eduardo Reway <eduardosouzar154@gmail.com>
2026-03-19 22:05:30 -0300

    Add files via upload

---
+++ celia aula 3/aula3_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2;
+
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+
+    return 0;
+}
