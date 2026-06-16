390734a0b818f972541f7b84074c5f2731828d4e
Eduardo Reway <eduardosouzar154@gmail.com>
2026-06-16 19:22:49 -0300

    Add files via upload

---
+++ enem/formulas_enem.cpp
@@ -0,0 +1,51 @@
+// CAMADA DE APRESENTACAO - 1 - FRONT END
+#include <iostream>
+
+#include "Matematica.h"
+#include "Fisica.h"
+
+using namespace std;
+
+int main() {
+
+    int opcao;
+
+    do {
+        cout << "\n====== FORMULAS ENEM ======\n";
+        cout << "1 - Matematica\n";
+        cout << "2 - Fisica\n";
+        cout << "0 - Sair\n";
+
+        cout << "Escolha: ";
+        cin >> opcao;
+
+        Formula* formula = nullptr; // ele sinaliza que o dado 
+        // está intencionalmente ausente ou vazio
+
+        switch(opcao) {
+
+            case 1:
+                formula = new Matematica();
+                break;
+
+            case 2:
+                formula = new Fisica();
+                break;
+
+            case 0:
+                cout << "Encerrando...\n";
+                break;
+
+            default:
+                cout << "Opcao invalida!\n";
+        }
+
+        if(formula != nullptr) {
+            formula->calcular();
+            delete formula;
+        }
+
+    } while(opcao != 0);
+
+    return 0;
+}
